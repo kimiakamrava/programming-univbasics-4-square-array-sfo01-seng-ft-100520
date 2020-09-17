ae3a84fc7b2f9b3704ce8bd239f9788477e831bd
kimia kamrava <kimiakamrava93@gmail.com>
2020-09-17 17:58:24 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array= [1,2,3])
-  
+  new_array = []
+  counter = 0 
+  while counter < numbers.length()
+  new_array.push(numbers[counter] * numbers[counter])
+  counter += 1
+  end 
+  return new_array
+end
 end
